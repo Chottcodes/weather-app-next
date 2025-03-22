693d1a4148fa34dff4538e78099997e3dadafa66
Chottcodes <cwellshott@gmail.com>
2025-03-21 18:07:31 -0700

    refactor api call

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,16 +1,16 @@
 const GetWeather = async (city:string) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_WEATHER_API_URL}?q=${city}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=imperial`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=imperial`);
     const data = await response.json();
     return data;
 }
 const GetFiveDayForcast = async (cityName:string) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_WEATHER_FIVE_DAY_API_URL}?q=${cityName}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=imperial`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=imperial`);
     const data = await response.json();
     return data;
 }
 const GetWeatherByCoordinates = async (lat: number, lon: number) => {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_WEATHER_API_URL}?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=imperial`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=imperial`
     );
     const data = await response.json();
     return data;
